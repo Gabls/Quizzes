4899aeb52f5fd6431f5a2206e4529899d23baf99
Gabriel Santos de Sousa <gabsousa.contato@gmail.com>
2022-03-26 20:19:21 -0300

    feat: add "add-quiz"

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { ServidorService } from 'src/app/services/servidor.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +9,12 @@ import { ServidorService } from 'src/app/services/servidor.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private servidor: ServidorService, private router: Router){
+  constructor(private servidor: ServidorService){
     let json = JSON.parse(sessionStorage.getItem("user") || "{}");
     this.token = json["token"];
     this.nome = json["user"]["name"];
 
-    this.servidor.get("https://h-api-ava.tindin.com.br/quizzes?filter=team:623497e07ccb72a54717b9f4&fields=name,title,description,level,rewardXp,type", this.token).subscribe(res=>{
-      this.lista = res;
-      this.lista = this.lista["quizzes"];
-      console.log(this.lista);
-    });
+    this.load();
   }
 
   ngOnInit(): void {
@@ -28,4 +24,16 @@ export class HomeComponent implements OnInit {
   public nome:string = "";
 
   public lista:any = [];
+
+  load(){
+    this.servidor.get("https://h-api-ava.tindin.com.br/quizzes?filter=team:623497e07ccb72a54717b9f4&fields=name,title,description,level,rewardXp,type", this.token).subscribe(res=>{
+      this.lista = res;
+      this.lista = this.lista["quizzes"];
+    });
+  }
+
+  adicionar(){
+    $("#background").fadeToggle();
+    $("#form").animate({width:'toggle'},450);
+  }
 }
